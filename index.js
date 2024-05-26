8720ab9ce82ee208291c216ddc37a894bc5d8b07
Maikon Monteiro <maikonsm7@gmail.com>
2024-05-26 16:05:44 -0400

    final template engine

--- index.js
+++ index.js
@@ -4,12 +4,30 @@ const port = 3000
 
 const app = express()
 
+app.use(express.static('public'))
+
 //configuração handlebars
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
 
 app.get('/', (req, res) => {
-    res.render('home')
+    const nome = 'Maikon'
+    const auth = true
+    res.render('home', {nome, auth})
+})
+
+app.get('/dashboard', (req, res) => {
+    const numeros = [1, 2, 3]
+    res.render('dashboard', {numeros})
+})
+
+app.get('/posts', (req, res) => {
+    const posts = [
+        {title: 'React', category: 'Developer'},
+        {title: 'BootStrap', category: 'Framework'},
+        {title: 'NodeJS', category: 'Backend'}
+    ]
+    res.render('posts', {posts})
 })
 
 app.listen(port, () => {
